487226dcc95fb1d011dfdb68c51c1a4dc17791fc
Kejal Bhuva <Kejal.Bhuva@codeimmersives.com>
2019-12-12 13:01:46 -0500

    tried my best

--- main.js
+++ main.js
@@ -1,46 +1,129 @@
-const makeParagraph = () => {
+const makeParagraph = (arr) => {
+  let newStr = ''
+  for (let i=0;i<arr.length;i++) {
+    if (i<arr.length-1) {
+      newStr = newStr +  arr[i] + ' '
+    } else {
+      newStr = newStr +  arr[i]
+    }
+  }
+  return newStr
+}
+
+
+const averageDogAge = (pets) => {
+  let newArr = []
+  const blahs = pets.filter((pet) => pet.species === 'dog')
+  for (const blah of blahs) {
+
+    blah.forEach((blah) => newArr.push(blah.age))
+  }
+  return newArr
+}
+
+
+const addToDigits = (str,num) => {
+str = str.toString()
+let newStr = ''
+for (let i=0; i<str.length; i++) {
+  newStr += `${str[i] + num}`
+}
+return newStr  
+}
+
+console.log(addToDigits(111,1))
+
+
+const nightOwls = (nights) => {
+  return nights.filter((night) => night.localTime >= 100 && night.localTime <= 400 && night.asleep === false )
 
 }
 
-const averageDogAge = () => {
+const nap = (nights) => {
+  // const blah = nights.filter((night) => night.localTime >= 100 && night.localTime <= 400 && !night.asleep)
+  // !blah.asleep === blah.asleep
+  // return blah
+  // const blah = nights.map((night) => nightOwls(night))
+  // return blah = blah.map((night) => night.asleep === true )
+  // const blah = nights.filter((night) => night.localTime >= 100 && night.localTime <= 400 && night.asleep === false )
+  return nights.filter((night) => night.localTime >= 100 && night.localTime <= 400).map((night)=> night.asleep===false ? night.asleep=== true : night.asleep=== true)
 
 }
 
-const addToDigits = () => {
-
-}
-
-const nightOwls = () => {
-
-}
-
-const nap = () => {
-
-}
-
-const findIndices = () => {
-
+const findIndices = (arr,func) => {
+  let newArr = []
+  for (let i=0; i< arr.length; i++) {
+    if (func(arr[i])) {
+      newArr.push(i)
+    }
+  }
+  return newArr
 }
 
 const Faqtory = () => {
+  return {
+    questions: [],
+    length: 0, 
+
+    addQuestion: function (question) {
+      this.questions.push(this.question.text)
+      this.questions.length++
+    },
+
+    answerQuestion: function () {
+
+    },
+
+  } 
 
 }
 
 
 class Dog {
+  constructor(name,breed,color) {
+    this.name = name;
+    this.breed = breed;
+    this.color = color;
+  }
+
+  makeNoise () {
+    return 'Woof.'
+  }
+}
+
+class GreatDane extends Dog {
+  constructor(name, color) {
+    super(name,'Great Dane', color) 
+  }
+
+  makeNoise() {
+    return 'WOOF.'
+  }
 
 }
 
-class GreatDane {
+class Beagle extends Dog {
+  constructor(name) {
+    super(name, 'Beagle', 'white/black/brown')
+  }
+
+  makeNoise () {
+    return 'Awooooooooo.'
+  }
 
 }
 
-class Beagle {
-
-}
-
-class Pointer {
-
+class Pointer extends Dog {
+  constructor(name, color, points) {
+    super(name, 'Pointer', color)
+  }
+  points() {
+  if (points) {
+    return 'Over there!'
+  } else {
+    return 'Woof.'
+  }
+  }
 }
 
 
